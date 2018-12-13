e2cd6cee208e6c2e66bec26b4a7c34f532901515
Bobby Wibowo <bobbywibowo98@gmail.com>
2018-12-14 02:12:20 +0700

    Updated Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 
 public class Main {
 
+    // TODO: GUI
+
     public static void main(String[] args) {
         String filePath = "";
         if (args.length > 0 && !args[0].isEmpty()) {
